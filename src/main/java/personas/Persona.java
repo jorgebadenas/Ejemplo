67cb3522a50fcfb68586a6eb2f41b3db5c913cdb
jorgebadenas <badenas@uji.es>
2017-02-20 21:16:46 +0100

    Segundo commit

--- src/main/java/personas/Persona.java
+++ src/main/java/personas/Persona.java
@@ -1,8 +1,48 @@
 package personas;
 
+import java.time.LocalDateTime;
+
 public class Persona {
 	private String nombre;
 	private String dni;
+	private LocalDateTime nacimiento;
+	/**
+	 * @param nombre
+	 * @param dni
+	 * @param nacimiento
+	 */
+	public Persona(String nombre, String dni, LocalDateTime nacimiento) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+		this.nacimiento = nacimiento;
+	}
+	
+	public Persona() { }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public LocalDateTime getNacimiento() {
+		return nacimiento;
+	}
+
+	public void setNacimiento(LocalDateTime nacimiento) {
+		this.nacimiento = nacimiento;
+	}
 	
 
 }
